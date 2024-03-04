2030f197a66da68e395a3d84487c8483ca6f27f6
Beacon6 <61936260+Beacon6@users.noreply.github.com>
2024-03-04 16:28:26 +0100

    Add Fugitive plugin

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,13 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  {
+    'tpope/vim-fugitive',
+    opts = {},
+    config = function()
+      vim.keymap.set('n', '<leader>gs', vim.cmd.Git)
+      vim.keymap.set('n', '<leader>gd', vim.cmd.Gdiffsplit)
+    end,
+  },
+}
