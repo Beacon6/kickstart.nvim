5a97312f01add7d6b3497ba18194a4e4b6578a87
Beacon6 <61936260+Beacon6@users.noreply.github.com>
2024-03-17 14:33:53 +0100

    Add keymap descriptions for Harpoon

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -26,22 +26,22 @@ return {
 
       vim.keymap.set('n', '<leader>a', function()
         harpoon:list():append()
-      end)
+      end, { desc = '[A]dd to Harpoon list' })
       vim.keymap.set('n', '<leader>`', function()
         harpoon.ui:toggle_quick_menu(harpoon:list())
-      end)
+      end, { desc = 'Toggle Harpoon menu' })
       vim.keymap.set('n', '<leader>1', function()
         harpoon:list():select(1)
-      end)
+      end, { desc = 'Go to file [1]' })
       vim.keymap.set('n', '<leader>2', function()
         harpoon:list():select(2)
-      end)
+      end, { desc = 'Go to file [2]' })
       vim.keymap.set('n', '<leader>3', function()
         harpoon:list():select(3)
-      end)
+      end, { desc = 'Go to file [3]' })
       vim.keymap.set('n', '<leader>4', function()
         harpoon:list():select(4)
-      end)
+      end, { desc = 'Go to file [4]' })
     end,
   },
 }
